5d585c53d9d8aa232e9ea0d8d6ef833262f8ca37
230701243 <230701243@rajalakshmi.edu.in>
2024-06-14 22:19:23 +0530

    Update 2.1.py

--- Week 2/2.1.py
+++ Week 2/2.1.py
@@ -1,16 +1,8 @@
-The program that you create for this exercise will begin by reading the cost of a meal ordered at a restaurant from the user. Then your program will compute the tax and tip for the meal. Use your local tax rate (5 percent) when computing the amount of tax owing. Compute the tip as 18 percent of the meal amount (without the tax). The output from your program should include the tax amount, the tip amount, and the grand total for the meal including both the tax and the tip. Format the output so that all of the values are displayed using two decimal places.
-                                                                                                                                                                                                                                                                                                                                                                                                                                          
-a=int(input())
-tax=a*(5/100)
-tip=a*(18/100)
-total=tax+tip+a
-print("The tax is {:.2f} and the tip is {:.2f}, making the total {:.2f}" .format(tax,tip,total))
+Pretend that you have just opened a new savings account that earns 4 percent interest per year. The interest that you earn is paid at the end of the year, and is added to the balance of the savings account. Write a program that begins by reading the amount of money deposited into the account from the user. Then your program should compute and display the amount in the savings account after 1, 2, and 3 years. Display each amount so that it is rounded to 2 decimal places. Sample Input: 10000 Sample Output: Balance as of end of Year 1: $10400.00. Balance as of end of Year 2: $10816.00. Balance as of end of Year 3: $11248.64.
 
-Sample Input
-
-100
-
-Sample Output
-
-The tax is 5.00 and the tip is 18.00, making the total 123.00
+n=int(input(""))
+for i in range(1,4):
+    m=n*(4/100)
+    n=n+m
+    print("Balance as of end of Year ",i,": $%.2f."%(n),sep='')
 
