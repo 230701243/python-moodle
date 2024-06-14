ba722891be1037f8a077dc44f44f4a28ef251fbb
230701243 <230701243@rajalakshmi.edu.in>
2024-06-14 22:21:28 +0530

    Create 2.2.py

---
+++ Week 2/2.2.py
@@ -0,0 +1,35 @@
+Mr. X's birthday is in next month. This time he is planning to invite N of his friends. He wants to distribute some chocolates to all of his friends after the party. He went to a shop to buy a packet of chocolates. At the chocolate shop, 4 packets are there with different numbers of chocolates. He wants to buy such a packet which contains a number of chocolates, which can be distributed equally among all of his friends. Help Mr. X to buy such a packet.
+
+n=int(input())
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+if(a%n==0):
+     print("True",end=' ')
+else:
+     print("False",end=' ')
+if(b%n==0):
+     print("True",end=' ')
+else:
+     print("False",end=' ')
+if(c%n==0):
+     print("True",end=' ')
+else:
+     print("False",end=' ')
+if(d%n==0):
+     print("True",end=' ')
+else:
+     print("False",end=' ')
+
+SAMPLE INPUT AND OUTPUT:
+
+5
+
+25
+
+12  
+
+10  
+
+9
